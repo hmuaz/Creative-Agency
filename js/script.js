c2647f44dd3fa673f89dafcdb93fd4303e2adff2
hmuaz <muazsenol1@gmail.com>
2023-06-19 16:03:32 +0300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".toggle").addEventListener("click", () => {
+    document.querySelector(".toggle").classList.toggle("active");
+    document.querySelector(".navigation").classList.toggle("active");
+})
